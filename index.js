aef8d0743ef7d6000fe9fbc9375d3f78b77f8ee8
camille <camille.lazennec@ynov.com>
2025-02-26 11:31:19 +0100

    Jouer la fonction, supprimer la bulle, direction de la Bulle

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
-const bubble = document.createElement("span");
-bubble.classList.add("bubble");
-document.body.appendChild(bubble); 
+const bubbleMarker = () => {
+    const bubble = document.createElement("span");
+    bubble.classList.add("bubble");
+    document.body.appendChild(bubble); 
 
-const size = Math.random() * 200 + 100 + "px"; //Obtenir une bulle entre 100 et 300px
-bubble.style.height = size; //injecter la taille
-bubble.style.width = size;
+    const size = Math.random() * 200 + 100 + "px"; //Obtenir une bulle entre 100 et 300px
+    bubble.style.height = size; //injecter la taille
+    bubble.style.width = size;
 
-bubble.style.top = Math.random() * 100 + 50 + "%"; 
-bubble.style.left = Math.random() * 100 + 50 + "%"; 
+    bubble.style.top = Math.random() * 100 + 50 + "%"; 
+    bubble.style.left = Math.random() * 100 + 50 + "%"; 
 
-bubble.style.setProperty("--left", Math.random() * 100 + "%");
+    const plusMinus = Math.random() > 0.5 ? 1 : -1; //La bulle part à droite ou à gauche
+    bubble.style.setProperty("--left", Math.random() * 100 * plusMinus + "%");
+
+    setTimeout(() => { //supprime la bulle après 8s
+        bubble.remove();
+    }, 8000);
+};
+
+
+setInterval(bubbleMarker, 3000); //Fonction jouer tt les 3 seconde
+
