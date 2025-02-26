feb7cf571490a7dd948a4ba1fae885a325b48af8
camille <camille.lazennec@ynov.com>
2025-02-26 10:54:17 +0100

    bubble size

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 const bubble = document.createElement("span");
-document.body.appendChild(bubble);
+bubble.classList.add("bubble");
+document.body.appendChild(bubble); 
+
+const size = Math.randome() * 200 + 100 + "px"; //Obtenir une bulle entre 100 et 300px
+bubble.style.height = size; //injecter la taille
+bubble.style.width = size;
+;;;
+console.log(size);;
