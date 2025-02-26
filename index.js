7552389ec623120590fdf3ce060592c88b01b70f
camille <camille.lazennec@ynov.com>
2025-02-26 11:16:30 +0100

    bubble direction

--- index.js
+++ index.js
@@ -2,8 +2,11 @@ const bubble = document.createElement("span");
 bubble.classList.add("bubble");
 document.body.appendChild(bubble); 
 
-const size = Math.randome() * 200 + 100 + "px"; //Obtenir une bulle entre 100 et 300px
+const size = Math.random() * 200 + 100 + "px"; //Obtenir une bulle entre 100 et 300px
 bubble.style.height = size; //injecter la taille
 bubble.style.width = size;
-;;;
-console.log(size);;
+
+bubble.style.top = Math.random() * 100 + 50 + "%"; 
+bubble.style.left = Math.random() * 100 + 50 + "%"; 
+
+bubble.style.setProperty("--left", Math.random() * 100 + "%");
